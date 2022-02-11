0b63a504e87971029bfb56655660c0befee75d5a
Basile Lauriola <basile.lauriola@ens.uvsq.fr>
2022-02-11 15:03:08 +0100

    début

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -9,9 +9,12 @@
 
 # import des librairies
 
+import tkinter as tk
+import random
 
 # définition d'une constante (cap)
 
+CANVAS_WIDTH, CANVAS_HEIGHT = 500, 500
 
 # définition des variables globales
 
@@ -20,4 +23,21 @@
 
 
 # programme principal définition des widgets/événements
+
+root = tk.Tk()
+root.title("Mon dessin")
+
 # gestion des événements
+
+bouton1 = tk.Button(root, text="Aléatoire", font = ("helvetica", "10"), bg="pink"
+                  ) # création du widget
+bouton1.grid(row=2, column=0) # positionnement du widget
+
+canvas = tk.Canvas(root, bg="black", width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
+
+canvas.grid(row=1, column=0)
+
+# Fin de votre code
+
+
+root.mainloop()

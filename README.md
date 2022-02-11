369af942e2762b7774efa0bc1e1a8d9581963315
Basile Lauriola <basile.lauriola@ens.uvsq.fr>
2022-02-11 14:41:47 +0100

    First comit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # projet_tas_de_sable
 Projet tas de sable Basile Lauriola, Camilia Boukebouche, Julien Hrelja
+TD 3
+https://github.com/uvsq22107694/projet_tas_de_sable
+https://github.com/uvsq-info/l1-python
---
+++ tas_de_sable.py
@@ -0,0 +1,8 @@
+#########################################
+# groupe MI TD3
+# Basile Lauriola
+# Camilia Boukebouche
+# Julien Hrelja
+# https://github.com/uvsq22107694/projet_tas_de_sable
+# https://github.com/uvsq-info/l1-python
+#########################################

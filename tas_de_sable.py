cc88570bd7a43e9c603f12192624f63be6bb93bd
Basile Lauriola <basile.lauriola@ens.uvsq.fr>
2022-02-11 15:56:41 +0100

    Grille aléatoire

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -11,15 +11,32 @@
 
 import tkinter as tk
 import random
+from turtle import width
 
 # définition d'une constante (cap)
 
 CANVAS_WIDTH, CANVAS_HEIGHT = 500, 500
+GRILLE_WIDTH, GRILLE_HEIGHT = 100, 100
 
 # définition des variables globales
 
 
 # définition des fonctions (docstring)
+
+def aleatoire():
+
+    coul = ["#FF1900", "#FF3200", "#FF4B00",
+            "#FF6400", "#FF7D00", "#FF9600",
+            "#FFAF00", "#FFC800", "#FFE100"]
+
+    for i in range(GRILLE_WIDTH):
+
+        for h in range(GRILLE_HEIGHT):
+
+            couleur=coul[random.randint(0,8)]
+            
+            canvas.create_rectangle(i*(CANVAS_WIDTH//GRILLE_WIDTH),h*(CANVAS_WIDTH//GRILLE_WIDTH),i*(CANVAS_WIDTH//GRILLE_WIDTH)+5,h*(CANVAS_WIDTH//GRILLE_WIDTH)+5,fill=couleur,width=0)
+            
 
 
 # programme principal définition des widgets/événements
@@ -29,11 +46,11 @@ root.title("Mon dessin")
 
 # gestion des événements
 
-bouton1 = tk.Button(root, text="Aléatoire", font = ("helvetica", "10"), bg="pink"
+bouton1 = tk.Button(root, text="Aléatoire", font = ("helvetica", "10"), bg="pink",command=aleatoire
                   ) # création du widget
 bouton1.grid(row=2, column=0) # positionnement du widget
 
-canvas = tk.Canvas(root, bg="black", width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
+canvas = tk.Canvas(root, bg="black", width = CANVAS_WIDTH, height = CANVAS_HEIGHT, borderwidth=0)
 
 canvas.grid(row=1, column=0)
 

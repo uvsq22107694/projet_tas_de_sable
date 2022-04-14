cb37187203226dc1647ef14d14fc26df1a0cccb7
Basile Lauriola <basile.lauriola@ens.uvsq.fr>
2022-04-14 13:51:25 +0200

    commit1

--- brouillion.py
+++ brouillion.py
@@ -1,51 +1,29 @@
-import tkinter as tk
+from tkinter import *
+import time
 import random
 
-CANVAS_WIDTH, CANVAS_HEIGHT = 500, 500
-TAILLE_REC_X,TAILLE_REC_Y = 400, 400
 
-root = tk.Tk()
-root.title("Mon dessin")
-global coul,objets
-coul,freeze = False,False
-objets = []
+def xy(event): # unused...
+    xm, ym = event.x, event.y
 
-# Début de votre code
+def task():
+    w=random.randint(1,1000)
+    h=random.randint(1,1000)
+    canvas.create_rectangle(w,h,w+150,h+150)
+    tk.after_id = tk.after(1000,task)           
 
-def clic_rec(event):
-    global coul,freeze
-    
-    if(event.x < CANVAS_WIDTH-TAILLE_REC_X or event.y < CANVAS_HEIGHT-TAILLE_REC_Y \
-            or event.x > TAILLE_REC_X or event.y > TAILLE_REC_Y or freeze == True):
+def callback(event):
+    print("clicked2")    
+    if tk.after_id:
+        tk.after_cancel(tk.after_id)
+        tk.after_id = None
 
-        freeze=True
-    else:
-        if(coul == False):
-            canvas.itemconfigure(1,fill="blue")
-            coul = True
-        else:
-            canvas.itemconfigure(1,fill="red")
-            coul = False
+if __name__ == '__main__':
+    tk = Tk()
+    tk.after_id = None
+    canvas = Canvas(tk, width=200, height=200, background="grey")
+    canvas.pack()
+    canvas.bind("<Button-1>", callback)        
+    task() # tk.after(1000,task)
 
-def reset():
-    global freeze,coul
-    freeze,coul = False,False
-    canvas.itemconfigure(1,fill="red")
-    print("test")
-
-bouton1 = tk.Button(root, text="Recommencer", font = ("helvetica", "10"), bg="pink", command=reset
-                  ) # création du widget
-bouton1.grid(row=2, column=0) # positionnement du widget
-
-root.bind("<Button-1>", clic_rec)
-
-canvas = tk.Canvas(root, bg="black", width = CANVAS_WIDTH, height = CANVAS_HEIGHT)
-
-canvas.create_rectangle(100,100,400,400,fill="red")
-
-canvas.grid(row=1, column=0, rowspan=1, columnspan=1)
-
-# Fin de votre code
-
-
-root.mainloop()
+    tk.mainloop()   
